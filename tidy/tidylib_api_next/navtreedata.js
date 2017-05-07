99dbc7fce2fbaffbb46dd7f10c073cdf18eee509
Jim Derry <balthisar@gmail.com>
2017-05-07 15:23:26 -0400

    - Updated `next` to 5.5.19. - Also, links will now simply be `next` in order to prevent link-rot. We will   _not_ host every little version bump from next, but only major versions.

---
+++ tidy/tidylib_api_next/navtreedata.js
@@ -0,0 +1,26 @@
+var NAVTREE =
+[
+  [ "HTML Tidy", "index.html", [
+    [ "About HTML Tidy", "index.html", null ],
+    [ "Building HTML Tidy", "md_pages_BUILD.html", null ],
+    [ "License", "md_pages_LICENSE.html", null ],
+    [ "General", "general.html", "general" ],
+    [ "LibTidy", "libtidy_01.html", "libtidy_01" ],
+    [ "Programming", "programming.html", "programming" ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Modules", "modules.html", "modules" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"api_namespace.html",
+"group__messageobj__message__api.html#ga3f640506737650059b6ba66f6dbe3fd0",
+"group__public__enumerations.html#gga1686318b9a8c2aa6e60e341af7145c00a8f23224e70a04fde385db345ac8de8f0",
+"group__public__enumerations.html#gga3a1401652599150188a168dade7dc150a96fcf40116bdcd24ff0187d8781ca497",
+"group__public__enumerations.html#gga4cbfcbf234421459e736d62cfdd4c3e1a9c629d20357444632eea1c4b3658f8bc",
+"group__public__enumerations.html#ggae5b597c0999422ff383f08ab595ef5e8aac86fbc2dcb4d59627dbbd0aabdf6ea6"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
